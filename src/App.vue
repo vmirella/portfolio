aa20cec31ebab7d2e2a0328ee847c0f6aec1152f
vmirella <vmirella_17@hotmail.com>
2021-02-21 22:59:38 -0500

    (P-1) chore: install tailwind

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" class="mx-auto" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <p class="bg-purple-500">Hola</p>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
