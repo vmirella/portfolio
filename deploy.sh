303f582c3b469750ca506324744a440f8e29f6f8
vmirella <vmirella_17@hotmail.com>
2021-04-15 03:10:27 -0500

    (P-4) chore: change branch for deployment

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:vmirella/portfolio.git main:gh-pages
+git push -f git@github.com:vmirella/portfolio.git master:gh-pages
 
 cd -
